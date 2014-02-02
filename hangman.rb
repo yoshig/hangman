d361a84da2093a3003dc017fdebe399a70529056
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2014-02-01 18:59:11 -0800

    Created base classes

---
+++ hangman.rb
@@ -0,0 +1,14 @@
+class WordToGuess(word)
+  @uncovered = word.split()
+  @covered = 
+end
+
+class Hangman(player1, player2)
+
+end
+
+class HumanPlayer
+end
+
+class ComputerPlayer(iq = "dumb")
+end
